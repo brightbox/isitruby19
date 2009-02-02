eacca9c5e47199a355b2b3c6e90266d983c84454
Rahoul Baruah <rahoul@brightbox.co.uk>
2009-02-02 16:40:49 +0000

    removed dependency on authlogic, added desc to rake task

--- config/environment.rb
+++ config/environment.rb
@@ -1,9 +1,5 @@
 # Be sure to restart your server when you modify this file
 
-# Uncomment below to force Rails into production mode when
-# you don't control web/app server and can't set it the proper way
-# ENV['RAILS_ENV'] ||= 'production'
-
 # Specifies gem version of Rails to use when vendor/rails is not present
 RAILS_GEM_VERSION = '2.2.2' unless defined? RAILS_GEM_VERSION
 
@@ -28,7 +24,6 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
   
-   config.gem "authlogic"
    config.gem "ferret"
    config.gem "mislav-will_paginate", :lib => 'will_paginate', :source => 'http://gems.github.com/'
    config.gem "recaptcha", :source => 'http://www.loonsoft.com/recaptcha/pkg/'
--- lib/tasks/isitruby19.rake
+++ lib/tasks/isitruby19.rake
@@ -3,7 +3,8 @@ namespace :isitruby19 do
   task :import => :environment do
     GemImporter.import
   end
-  
+
+  desc "load the default set of platforms"  
   task :load_platforms => :environment do
     Platform.load_defaults
   end 
