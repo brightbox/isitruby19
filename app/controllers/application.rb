1d665abd5acf4708d4b329f2edab8214ee49078e
David Smalley <david@davidsmalley.com>
2009-02-04 20:28:13 +0000

    Don't protect from forgery - we have a captcha for that

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -2,7 +2,7 @@ gem 'recaptcha'
 
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
-  protect_from_forgery # :secret => 'f9bf17028b589db09b49a672a101e6c3'
+  # protect_from_forgery # :secret => 'f9bf17028b589db09b49a672a101e6c3'
   filter_parameter_logging :password
   layout 'isitruby19'
   include ReCaptcha::AppHelper
