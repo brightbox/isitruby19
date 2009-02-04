295621c7f12efc453d42236545f5b194a01e43c8
Jeremy Jarvis <jeremy@brightbox.co.uk>
2009-02-04 00:00:13 +0000

    more work on UI

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,8 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+function clear_search() {
+	if ($('s').value == 'gem search') {
+		$('s').value == "";
+	}	
+}
