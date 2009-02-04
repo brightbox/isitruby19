3974ad1abfd773880cd562058741ddad49066003
Rahoul Baruah <rahoul@brightbox.co.uk>
2009-02-04 12:03:25 +0000

    altered list of platforms, removed reference to code compatibility

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,6 +5,5 @@ class HomeController < ApplicationController
     @latest_feedback = Comment.latest.all(:limit => 5)
     @popular = Code.popular.all(:limit => 10)
     @unpopular = Code.unpopular.all(:limit => 5)
-    @compatibility = Code.compatibility
   end
 end
--- app/models/platform.rb
+++ app/models/platform.rb
@@ -6,20 +6,11 @@ class Platform < ActiveRecord::Base
   end
 
   def self.load_defaults
-    add_entry_for 'Mac OSX 10.5'
-    add_entry_for 'Mac OSX 10.4'
+    add_entry_for 'Mac OSX'
+    add_entry_for 'GNU/Linux'
+    add_entry_for 'Windows'
     add_entry_for 'Solaris'
-    add_entry_for 'Ubuntu Intrepid'
-    add_entry_for 'Ubuntu Hardy'
-    add_entry_for 'Ubuntu Dapper'
-    add_entry_for 'Fedora'
-    add_entry_for 'Suse'
-    add_entry_for 'Debian'
-    add_entry_for 'Gentoo'
-    add_entry_for 'RHEL'
-    add_entry_for 'Windows Vista'
-    add_entry_for 'Windows XP'
-    add_entry_for 'Windows 7'
+    add_entry_for 'other'
   end
   
 private
