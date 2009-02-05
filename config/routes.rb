02973e240abc131ed86a73da7d0a9873cd854af5
David Smalley <david@davidsmalley.com>
2009-02-05 20:45:48 +0000

    Added formatted routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :users
   
   map.resources :authors
+  
   map.code_by_slug "/:slug_name", :controller => "codes", :action => "show"
-
+  map.code_by_slug_with_format "/:slug_name.:format", :controller => "codes", :action => "show"
+  map.comments_by_code_with_format "/:code_id/comments.:format", :controller => "comments", :action => "index"
 end
