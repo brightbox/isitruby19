00e38609a436d4870537a18016fa20279a4c5a99
Rahoul Baruah <rahoul@brightbox.co.uk>
2009-02-09 12:29:15 +0000

    switched to active record sessions for better security with comments and sessions

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090205210702) do
+ActiveRecord::Schema.define(:version => 20090209122314) do
 
   create_table "authors", :force => true do |t|
     t.string   "name"
@@ -74,5 +74,15 @@ ActiveRecord::Schema.define(:version => 20090205210702) do
   end
 
   add_index "platforms", ["name"], :name => "index_platforms_on_name"
+
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
 end
--- lib/tasks/isitruby19.rake
+++ lib/tasks/isitruby19.rake
@@ -8,4 +8,9 @@ namespace :isitruby19 do
   task :load_platforms => :environment do
     Platform.load_defaults
   end 
+  
+  desc "Clear expired sessions"
+  task :clear_expired_sessions => :environment do
+    ActiveRecord::Base.connection.execute("delete from sessions where updated_at < '#{6.hours.ago.to_s(:db)}';")
+  end 
 end
