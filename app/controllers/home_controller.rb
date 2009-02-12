f98327fb2689a56f9229387873fe0b95a1cd10e9
Rahoul Baruah <rahoul@brightbox.co.uk>
2009-02-12 11:55:52 +0000

    remove delete link from home page and reinstate home page caching

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,8 @@
 class HomeController < ApplicationController
   def show
     @page_title = 'Community-powered gem compatibility for ruby 1.9'
-    @latest_feedback = Comment.latest.all(:limit => 5)
     unless read_fragment :controller => 'home', :action => 'show'
+      @latest_feedback = Comment.latest.all(:limit => 5)
       @popular = Code.popular.all(:limit => 15)
       @unpopular = Code.unpopular.all(:limit => 5)
     end
